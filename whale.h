7663ef25e53eab27eb47cdba1329af58de4d5cf3
Stephanie Tong <tong.stephanie_mylinh@yahoo.com>
2016-10-12 15:31:24 -0700

    Whale.h file

---
+++ whale.h
@@ -0,0 +1,14 @@
+#ifndef INCL_IOSTREAM
+#include <iostream>
+#endif
+
+void PrintWhale() {
+
+  std::cout << "
+     .-'
+'--./ /     _.---.
+'-,  (__..-`       \
+   \          .     |
+    `,.__.   ,__.--/
+      '._/_.'___.-`" << endl;
+}
